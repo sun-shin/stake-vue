6ac56eb2c32e750e51b3cacbc843517af98aea0d
sun-shin <sunhanshin2@EMAIL.com>
2020-10-30 14:09:20 -0500

    implemented authentication w/ loggedIn method

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,48 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <span v-if="!loggedIn()">
+        <router-link to="/signup">Signup</router-link> |
+      </span>  
+      <span v-if="!loggedIn()">
+        <router-link to="/login">Login</router-link> |
+      </span>
+      <span v-if="loggedIn()">
+        <router-link to="/logout">Logout</router-link> |
+      </span>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  created: function() {},
+  
+  methods: {
+    loggedIn: function() {
+      return localStorage.getItem("jwt");
+    },
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+  }
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
