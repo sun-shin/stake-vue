491686bddcc75ae087b32d4f7aba73ddf12fd4a5
sun-shin <sunhanshin2@gmail.com>
2020-11-17 14:33:21 -0600

    integrate theme into Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,74 @@
 <template>
   <div class="login">
-    <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
+    <!-- PAGE CONTENT
+    ============================== -->
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="body-plain__title">
+            <h1 class="text-center">Sign In</h1>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
+      <!-- / .row -->
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-sm-offset-3 col-md-offset-3 col-lg-offset-4"
+        >
+          <div class="body-plain__form">
+            <form role="form" v-on:submit.prevent="submit()">
+              <ul>
+                <li class="text-danger" v-for="error in errors">{{ error }}</li>
+              </ul>
+              <label for="sign-in__email" class="sr-only">Enter email</label>
+              <div class="input-group">
+                <span class="input-group-addon"
+                  ><i class="fa fa-user"></i
+                ></span>
+                <input
+                  type="email"
+                  class="form-control"
+                  v-model="email"
+                  id="sign-in__email"
+                  placeholder="Enter email"
+                />
+              </div>
+              <br />
+              <label for="sign-in__password" class="sr-only"
+                >Enter password</label
+              >
+              <div class="input-group">
+                <span class="input-group-addon"
+                  ><i class="fa fa-lock"></i
+                ></span>
+                <input
+                  type="password"
+                  class="form-control"
+                  v-model="password"
+                  id="sign-in__password"
+                  placeholder="Password"
+                />
+              </div>
+              <br />
+
+              <input
+                type="submit"
+                class="btn btn-primary btn-block btn-lg"
+                value="Submit"
+              />
+            </form>
+
+            <!-- Sign Up link -->
+            <hr />
+            <p>Not registered? <a href="/signup">Create an Account.</a></p>
+          </div>
+          <!-- / .body-plain__form -->
+        </div>
       </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+      <!-- / .row -->
+    </div>
+    <br />
+    <!-- / .container -->
   </div>
 </template>
 
